28f75d9023b324c4ea83b2dc92f8a9ff4fdf3e71
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-04-13 17:00:50 -0600

    Initial Commit

---
+++ Informant/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Informant
+//
+//  Created by Ty Irvine on 2021-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
