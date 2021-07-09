28c17e145c700b1e7d4eeb9a914d92a72747fef8
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-07-08 21:00:03 -0600

    Hopefully fixed the menubar utility!

--- Informant/Helpers/Utility Helpers/MenubarUtilityHelper.swift
+++ Informant/Helpers/Utility Helpers/MenubarUtilityHelper.swift
@@ -11,6 +11,9 @@ import SwiftUI
 /// This is just a collection of static functions to help with manipulating the menubar utility
 class MenubarUtilityHelper {
 	
+	/// Contains the size of the selection
+	static var sizeAsString: String = ""
+	
 	/// Update utility size
 	static func updateSize(_ statusItem: NSStatusItem) {
 		
@@ -23,61 +26,70 @@ class MenubarUtilityHelper {
 			return
 		}
 		
-		// Reacts to the state of the selection
-		switch checkedSelection.state {
-		case .errorSelection: wipeMenubarInterface(statusItem)
-			return
-			
-		case .duplicateSelection:
-			return
-			
-		case .uniqueSelection:
-			break
-		}
-		
-		/// Path selection
-		guard let selection = checkedSelection.selection.paths else {
+		// Error selection found
+		if checkedSelection.state == .errorSelection {
 			wipeMenubarInterface(statusItem)
 			return
 		}
 		
-		// Make sure selection is only one item. Any more and we wipe the interface
-		if selection.count > 1 {
-			wipeMenubarInterface(statusItem)
+		// Duplicate selection found
+		else if checkedSelection.state == .duplicateSelection {
+			updateMenubarInterface(statusItem)
 			return
 		}
 		
-		// Get URL
-		let url = URL(fileURLWithPath: selection[0])
-		
-		// Keys
-		let keys: Set<URLResourceKey> = [
-			.totalFileSizeKey,
-			.isDirectoryKey
-		]
-		
-		// Get the resources
-		if let resources = SelectionHelper.getURLResources(url, keys) {
-			
-			// End execution if it's a directory
-			if resources.isDirectory == true {
+		// Unique selection found
+		else {
+			/// Path selection
+			guard let selection = checkedSelection.selection.paths else {
 				wipeMenubarInterface(statusItem)
 				return
 			}
-
-			// Get the size
-			else if let fileSize = resources.totalFileSize {
-				size = SelectionHelper.formatBytes(Int64(fileSize))
+		
+			// Make sure selection is only one item. Any more and we wipe the interface
+			if selection.count > 1 {
+				wipeMenubarInterface(statusItem)
+				return
 			}
-		}
 		
-		// Format size as string
-		guard let size = size else {
-			wipeMenubarInterface(statusItem)
-			return
-		}
+			// Get URL
+			let url = URL(fileURLWithPath: selection[0])
 		
-		let sizeAsString = size + "    "
+			// Keys
+			let keys: Set<URLResourceKey> = [
+				.totalFileSizeKey,
+				.isDirectoryKey
+			]
+		
+			// Get the resources
+			if let resources = SelectionHelper.getURLResources(url, keys) {
+			
+				// End execution if it's a directory
+				if resources.isDirectory == true {
+					wipeMenubarInterface(statusItem)
+					return
+				}
+
+				// Get the size
+				else if let fileSize = resources.totalFileSize {
+					size = SelectionHelper.formatBytes(Int64(fileSize))
+				}
+			}
+		
+			// Format size as string
+			guard let size = size else {
+				wipeMenubarInterface(statusItem)
+				return
+			}
+		
+			sizeAsString = size + "    "
+		
+			updateMenubarInterface(statusItem)
+		}
+	}
+	
+	/// Updates the status item
+	static func updateMenubarInterface(_ statusItem: NSStatusItem) {
 		
 		// Get formatted font ready
 		let font = NSFont.systemFont(ofSize: 13, weight: .medium)
@@ -89,6 +101,7 @@ class MenubarUtilityHelper {
 	
 	/// For when there's no size available
 	static func wipeMenubarInterface(_ statusItem: NSStatusItem) {
+		sizeAsString = ""
 		statusItem.button?.attributedTitle = NSAttributedString(string: "")
 	}
 }
