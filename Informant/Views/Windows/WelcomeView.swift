6d46318b7e54b84a7926002ec3abe0777d7d946a
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-07-18 16:05:53 -0600

    Finished up welcome view!

--- Informant/Views/Windows/WelcomeView.swift
+++ Informant/Views/Windows/WelcomeView.swift
@@ -13,6 +13,13 @@ struct WelcomeView: View {
 	
 	private let confettiFrameSize: CGFloat = 140
 	
+	/// This is the radius of the actual help image
+	private let helpImageRadius: CGFloat = 10.0
+	
+	/// Pop a new image in, adjust this scale and you'll be ready to go.
+	/// The image is anchored to the top so it'll relocate automatically sort to speak.
+	private let helpImageScale: CGFloat = 1.6
+	
 	@ObservedObject public var interfaceState: InterfaceState
 	
 	var body: some View {
@@ -27,7 +34,7 @@ struct WelcomeView: View {
 					.offset(y: 25)
 					.rotationEffect(Angle(degrees: -8))
 					.mask(
-						LinearGradient(gradient: Gradient(colors: [.black, .black.opacity(0)]), startPoint: UnitPoint(x: 0.5, y: 0.53), endPoint: UnitPoint(x: 0.5, y: 0.58))
+						LinearGradient(gradient: Gradient(colors: [.black, .black.opacity(0)]), startPoint: UnitPoint(x: 0.5, y: 0.54), endPoint: UnitPoint(x: 0.5, y: 0.58))
 					)
 					.frame(width: confettiFrameSize, height: confettiFrameSize, alignment: .center)
 					
@@ -48,7 +55,16 @@ struct WelcomeView: View {
 			Spacer().frame(height: Style.Padding.welcomeWindow)
 				
 			// How-to-use image
-				
+			Image("welcome-howto(BigSur)")
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.frame(width: 275, height: 145, alignment: .top)
+				.scaleEffect(helpImageScale, anchor: .top)
+				.cornerRadius(helpImageRadius)
+				.padding(1)
+				.background(Color.primary.opacity(0.25).cornerRadius(helpImageRadius + 1))
+				.padding(.bottom, 5)
+
 			Divider().frame(height: Style.Padding.welcomeWindow + 10)
 			
 			// Options stack
